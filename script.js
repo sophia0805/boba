4114c6b4d33c0c10fce2820c81a81da08929039b
sophia0805 <sophiawang8509@gmail.com>
2024-12-25 13:01:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const globe = document.getElementById('snowGlobe');
+const snowflakes = document.querySelectorAll('.snow');
+
+globe.addEventListener('click', function() {
+    const globeElement = this.querySelector('.globe');
+    const snowGlobe = document.querySelector('.snowGlobe');
+
+    snowGlobe.classList.remove('shaking');
+    void globeElement.offsetWidth;
+    snowGlobe.classList.add('shaking');
+
+    globeElement.classList.remove('snow-active');
+    void globeElement.offsetWidth;
+    globeElement.classList.add('snow-active');
+
+
+
+});
