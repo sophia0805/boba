d1a8629bdf1f9d001c67fcc05a37a69d8b878e8f
sophia0805 <sophiawang8509@gmail.com>
2024-12-25 14:35:24 -0500

    finall

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ globe.addEventListener('click', function() {
     void globeElement.offsetWidth;
     globeElement.classList.add('snow-active');
 });
+
+const tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: 'o6wJSwbKbds',
+        playerVars: {
+            'autoplay': 1,
+            'controls': 1,
+            'playsinline': 1,
+            'enablejsapi': 1
+        },
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    // Need user interaction first
+    document.addEventListener('click', function() {
+        event.target.playVideo();
+        event.target.unMute();
+        event.target.setVolume(50);
+    }, { once: true });
+}
